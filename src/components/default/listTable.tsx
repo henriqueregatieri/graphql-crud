83017f5b73d39df12bbf485b6dadfec97ff67b5c
Henrique Regatieri <riq.regatieri@gmail.com>
2021-06-28 12:28:16 -0300

    Add product listing

---
+++ src/components/default/listTable.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const StyledListTable = styled.table`
+  border: 1px solid gray;
+  border-collapse: collapse;
+
+  th,
+  td {
+    border: 1px solid gray;
+    text-align: left;
+    padding: 3px 18px 3px 6px;
+  }
+`;
--- src/components/products/List.tsx
+++ src/components/products/List.tsx
@@ -1,7 +1,38 @@
+import { ProductsConnector } from '../../connectors/graphql/products';
+import { ProductType } from '../../types/Product';
+import { StyledListTable } from '../default/listTable';
+
 export const ProductsList: React.FC = () => {
+  const { useGetAll } = ProductsConnector();
+  const { data, error, loading } = useGetAll();
+
+  const ProductsList = (
+    <StyledListTable>
+      <tr>
+        <th>Name</th>
+        <th>Description</th>
+        <th>Edit</th>
+        <th>Delete</th>
+      </tr>
+      {data?.Products.map((product: ProductType, key: number) => {
+        return (
+          <tr key={key}>
+            <td>{product.name}</td>
+            <td>{product.description}</td>
+            <td>Edit</td>
+            <td>Delete</td>
+          </tr>
+        );
+      })}
+    </StyledListTable>
+  );
+
   return (
     <div>
       <h1>Products List</h1>
+      {loading && <div>Loading</div>}
+      {error && <div>Error: {error}</div>}
+      {data && ProductsList}
     </div>
   );
 };
